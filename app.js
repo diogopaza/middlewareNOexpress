39b130c15ce744a9251c1d93e9f79987992e28b4
Diogo Paza <diogopazacvel@gmail.com>
2018-08-11 18:18:21 -0300

    final

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use( (req, res, next) => {
+
+  console.log('will run before any route')
+  next()
+
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
